cd590538e99072d2d2eba78d2170ef14a6b62d6e
romanstaff04 <romandariostaffolani@gmail.com>
2025-11-26 16:25:23 -0300

    interfaz grafica

--- salud.py
+++ salud.py
@@ -3,6 +3,10 @@ import glob
 import os
 import re
 from send2trash import send2trash
+import tkinter as tk
+from tkinter import messagebox
+import threading
+import sys
 
 CARPETA_CANALIZADOR = "canalizador"
 IATAS_VALIDOS = ["BUE", "IBUE", "GBAS", "GBAO", "GBAN", "LPG"]
@@ -100,35 +104,25 @@ def manipularDatos(df):
 
     df["Equipo"] = df["Equipo"].astype(str).str.strip().str.upper()
     df["Nro. identificación pieza según cliente"] = df["Nro. identificación pieza según cliente"].astype(str).str.strip().str.upper()
-    #eliminar duplicados
     duplicados = df.duplicated(subset="Nro. identificación pieza según cliente", keep="first")
     df.loc[duplicados, "Nro. identificación pieza según cliente"] = df.loc[duplicados, "Equipo"]
     df = df.drop_duplicates(subset=["Equipo"], keep="first")
 
-    #vaciar columnas
     df["Latitud"] = ""
     df["Longitud"] = ""
     df["Distrito Destino"] = ""
     df["Provincia"] = ""
     df["Atributo1"] = ""
-    #copiar columna AI en la columna Z
     df["Atributo1"] = df["Tipo"]
 
-    #lo que es envio, en columna "m", 10
     df.loc[df["Tipo"] == "Envio", "Tiempo espera"] = 10
 
     df.loc[df["Nombre Solicitante"] == "BIOMERIEUX ARGENTINA S.A.", "Hora Hasta"] = 1300
     df.loc[df["Nombre Solicitante"] == "GOBIERNO DE LA CIUDAD DE BUENOS AIR", "Hora Hasta"] = 1300
     
-    #si es envio y la direccion contiene onetto hora 1100
     condicion_oneto = df["Dirección destino"].str.contains(r"onett?o", case=False, na=False)
     filtro = condicion_oneto & (df["Atributo1"] == "Envio")
     df.loc[filtro, "Hora Hasta"] = 1100
-
-    #si es org courrier y direccion contiene iriarte o lafayette, se borra esa fila.
-    """condicion_iriarte_lafayette = df["Dirección destino"].str.contains(r"iriarte|lafayette", case=False, na=False)
-    filtro = condicion_iriarte_lafayette & (df["Nombre Solicitante"] == "ORG COURIER ARG")
-    df = df.drop(df[filtro].index)"""
 
     def normalizar(texto):
         if pd.isna(texto):
@@ -138,7 +132,6 @@ def manipularDatos(df):
         texto = " ".join(texto.split())
         return texto
 
-    # Patrones flexibles
     patrones = [
         r"iriarte\s*3070",
         r"iriarte.*3070",
@@ -146,50 +139,39 @@ def manipularDatos(df):
     ]
     regex_combinado = "(" + "|".join(patrones) + ")"
 
-    # Crear una serie normalizada SOLO PARA LA CONDICIÓN (no se agrega al dataframe)
     direcciones_normalizadas = df["Dirección destino"].apply(normalizar)
-
-    # Condición sin añadir columnas
     condicion_iriarte = direcciones_normalizadas.str.contains(regex_combinado, na=False)
-
-    # Borrar filas
     df = df.drop(df[condicion_iriarte].index)
 
     df.loc[df["Atributo1"] == "Retiro", "Tiempo espera"] = 20
     df.loc[df["Atributo1"] == "Retiro", "Volumen"] = 0.05
     df.loc[df["Altura"] == 0, "Altura"] = ""
 
-    #clientes que se borran
     df = df.drop(df[df["Nombre Solicitante"] == "BOSTON SCIENTIFIC ARGENTINA S A"].index)
     df = df.drop(df[df["Nombre Solicitante"] == "REGISTRO NACIONAL DE LAS PERSONAS"].index)
     df = df.drop(df[df["Nombre Solicitante"] == "OCASA DISTRIBUCION POSTAL"].index)
     df = df.drop(df[df["Nombre Solicitante"] == "IBM Argentina S.R.L."].index)
 
-    #que contenga centra y vega, 1 en columna s
     contengaCentra = df["Destinatario"].str.contains(r"CENTRA|centra|Centra", case=False, na=False)
     contengaVega = df["Dirección destino"].str.contains(r"vega|VEGA|Vega", case=False, na=False)
     filtro4 = contengaCentra & contengaVega
     df.loc[filtro4, "Ruta Virtual"] = 1
 
-    #que contenga centra y vega, 1 en columna s
     contengaInaer = df["Destinatario"].str.contains(r"INAER|inaer|Inaer|ina|Ina", case=False, na=False)
     contengaArenales = df["Dirección destino"].str.contains(r"ARENALES|Arenales|arenales|aren|Aren", case=False, na=False)
     filtro5 = contengaInaer & contengaArenales
     df.loc[filtro5, "Ruta Virtual"] = 2
 
-    #que contenga MAFFEI y CERVI, 3 en columna s
     contengaMaffei = df["Destinatario"].str.contains(r"MAFFEI|maffei|Maffei|maffe|Maffe", case=False, na=False)
     contengaCervi = df["Dirección destino"].str.contains(r"CERVIÑO|cerviño|Cerviño|Cervino|cervino|cervi|CERVI|Cervi", case=False, na=False)
     filtro6 = contengaMaffei & contengaCervi
     df.loc[filtro6, "Ruta Virtual"] = 3
 
-    df["Altura"] = df["Altura"].astype(str) #lo pasa a text
-    df.loc[df["Altura"] == "nan", "Altura"] = "" #deja la celda vacia cuando tiene nan
-    df["Altura"] = df["Altura"].str[:-2] #borra los ultimos 2 digitos
+    df["Altura"] = df["Altura"].astype(str)
+    df.loc[df["Altura"] == "nan", "Altura"] = ""
+    df["Altura"] = df["Altura"].str[:-2]
 
-    #concatenar altura con direccion
     df["Dirección destino"] = df["Dirección destino"] + " " + df["Altura"]
-
 
     return df
 
@@ -199,9 +181,7 @@ def procesar():
         print("No hay archivos para procesar.")
         return
 
-    # Filtrar dataset solo a los IATAs permitidos
     df_completo = df_completo[df_completo["Destino"].isin(IATAS_VALIDOS)]
-
     df_total = pd.DataFrame()
 
     for iata in IATAS_VALIDOS:
@@ -209,31 +189,26 @@ def procesar():
         if df.empty:
             continue
 
-
         df = manipularDatos(df)
         df = canalizadorLocalidad(df)
         df = canalizadorProvincia(df)
-        #el normalizador de direccion funciona en las zonas que NO sean LA PLATA
+
         condicion_laPlata = df["Distrito Destino"] != "LA PLATA"
         df.loc[condicion_laPlata, "Dirección destino"] = (
-        df.loc[condicion_laPlata, "Dirección destino"]
-        .apply(ordenar_y_corregir_direccion)
-)
-        
+            df.loc[condicion_laPlata, "Dirección destino"]
+            .apply(ordenar_y_corregir_direccion)
+        )
 
-        #capital federal y horaDesde >= a 1400, 502
         df["Distrito Destino"] = df["Distrito Destino"].str.strip()
         condicionProvincia = df["Distrito Destino"] == "CAPITAL FEDERAL"
         condicionHora = df["Hora Desde"] >= 1400
         filtro2 = condicionProvincia & condicionHora
         df.loc[filtro2, "Ruta Virtual"] = 502
 
-        #lo que sea retiro en capital federal y cliente gobierno, 600 en ruta de accion
         condicionProvincia = df["Distrito Destino"] == "CAPITAL FEDERAL"
         condicionCliente = df["Nombre Solicitante"] == "GOBIERNO DE LA CIUDAD DE BUENOS AIR"
         filtro3 = condicionProvincia & condicionCliente
         df.loc[filtro3, "Ruta Virtual"] = 600
-
 
         df_total = pd.concat([df_total, df], ignore_index=True)
 
@@ -241,13 +216,51 @@ def procesar():
         print("No se generaron datos válidos.")
         return
 
-    #borrarMHTML()
+    borrarMHTML()
     #borrarXLSX()
     nombre_salida = "subirUnigis-SALUD.xlsx"
     df_total.to_excel(nombre_salida, index=False)
     os.startfile(nombre_salida)
     print("Proceso finalizado:", nombre_salida)
 
+# ---------------------------
+#   INTERFAZ GRÁFICA
+# ---------------------------
 
-if __name__ == "__main__":
-    procesar()
+def ejecutar_proceso():
+    try:
+        procesar()
+        ventana.after(0, ventana.destroy)   # Cerrar ventana desde el hilo principal
+    except Exception as e:
+        ventana.after(0, lambda: messagebox.showerror("Error", f"Ocurrió un error:\n{e}"))
+
+def ejecutar_en_thread():
+    hilo = threading.Thread(target=ejecutar_proceso)
+    hilo.start()
+    sys.exit()
+
+ventana = tk.Tk()
+ventana.title("Procesador de Canalizador")
+ventana.geometry("400x200")
+ventana.resizable(False, False)
+
+frame = tk.Frame(ventana)
+frame.pack(expand=True)
+
+boton = tk.Button(
+    frame,
+    text="Procesar",
+    font=("Arial", 18, "bold"),
+    width=10,
+    height=1,
+    command=ejecutar_en_thread
+)
+
+# --- Footer ---
+footer = tk.Label(ventana, text="SALUD -- Ruteo Centralizado.", 
+                font=("Arial", 9), fg="gray")
+footer.pack(side="bottom", pady=5)
+
+boton.pack()
+
+ventana.mainloop()
